27f350fc9e8d26212b149e8a92d9e8d80e54a8a9
LiYanan07 <yanan.li07@ele.me>
2019-01-07 19:37:13 +0800

    modify blag-backend

--- blog-backend/routes/blog.js
+++ blog-backend/routes/blog.js
@@ -3,15 +3,13 @@ const db = require('../db')
 const Tips = require('../utils/tip')
 // const Utils = require('../utils/index')
 
-
 /**
  * get title & id
  */
 router.get('/api/blog/getTitle', async (ctx, next) => {
-
   let sql = 'select title,id from blog;'
   let value = ''
-  await db.query(sql,value).then(res => {
+  await db.query(sql, value).then(res => {
     if (res && res.length > 0) {
       ctx.body = {...Tips[0], data: res}
     } else {
@@ -28,63 +26,62 @@ router.get('/api/blog/getTitle', async (ctx, next) => {
  */
 router.get('/api/blog/getcontent', async (ctx, next) => {
   let data = ctx.query
-    let { id } = data
-    let sql = 'select * from blog where id = ?;'
-    let value = [id]
-    await db.query(sql,value).then(res => {
-      if (res && res.length > 0) {
-        ctx.body = {...Tips[0], data: res}
-      } else {
-        ctx.body = Tips[1005]
-      }
-    }).catch(e => {
-      console.log(666)
+  let { id } = data
+  let sql = 'select * from blog where id = ?;'
+  let value = [id]
+  await db.query(sql, value).then(res => {
+    if (res && res.length > 0) {
+      ctx.body = {...Tips[0], data: res}
+    } else {
       ctx.body = Tips[1005]
-    })
+    }
+  }).catch(e => {
+    console.log(666)
+    ctx.body = Tips[1005]
   })
+})
 /**
  * add note
  */
-router.post('/api/blog/add',async (ctx,next)=>{
+router.post('/api/blog/add', async (ctx, next) => {
   let data = ctx.request.body
   console.log(data)
   let { title, content, markdown_content, id } = data
 
-  if (id){
+  if (id) {
     let sql = `update blog set title=?, content=?,markdown_content=? where id = ?`,
-    value = [title, content, markdown_content,id];
+      value = [title, content, markdown_content, id]
     await db.query(sql, value).then(res => {
-        let {insertId} = res;
-        console.log(res)
-        if (insertId === 0 ) {
-            ctx.body = {
-                ...Tips[0],
-                data: {
-                    id
-                }
-            }
-        } else {
-            ctx.body = Tips[1002]
+      let {insertId} = res
+      console.log(res)
+      if (insertId === 0) {
+        ctx.body = {
+          ...Tips[0],
+          data: {
+            id
+          }
         }
+      } else {
+        ctx.body = Tips[1002]
+      }
     }).catch(e => {})
-  }else{
+  } else {
     let sql = `INSERT INTO blog(title,content,markdown_content) VALUES(?,?,?)`,
-    value = [title, content, markdown_content];
+      value = [title, content, markdown_content]
     await db.query(sql, value).then(res => {
-        let {insertId: id} = res;
-        if (id) {
-            ctx.body = {
-                ...Tips[0],
-                data: {
-                    id
-                }
-            }
-        } else {
-            ctx.body = Tips[1002]
+      let {insertId: id} = res
+      if (id) {
+        ctx.body = {
+          ...Tips[0],
+          data: {
+            id
+          }
         }
+      } else {
+        ctx.body = Tips[1002]
+      }
     }).catch(e => {})
   }
-
 })
 
 module.exports = router
